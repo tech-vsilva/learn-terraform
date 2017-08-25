13cea5cadbb3a03be89b2f7eb7c46c94fcf28b6e
Valter Silva <valter.silva@orderbird.com>
2017-08-25 14:38:09 +0200

    Install terraform binary in /usr/local/bin/ folder, but make possible to maintain different binary versions

--- terraform.sh
+++ terraform.sh
@@ -2,13 +2,24 @@
 
 set -x
 
-REMOTE_ZIP="https://releases.hashicorp.com/terraform/0.10.2/terraform_0.10.2_linux_amd64.zip"
-TEMP_FOLDER=$(mktemp -d)
-TERRAFORM_INSTALL_FOLDER="/opt/terraform"
+# Install and configure Terraform with a specific version
 
-TERRAFORM_VERSION=$(echo ${REMOTE_ZIP} | sed 's,/, ,g' | awk '{print $4}')
+TEMP_FOLDER=$(mktemp -d)
+
+TF_ZIP="https://releases.hashicorp.com/terraform/0.10.2/terraform_0.10.2_linux_amd64.zip"
+TF_BINARIES_FOLDER="/opt/hashicorp/terraform"
+TF_VERSION=$(echo ${TF_ZIP} | sed 's,/, ,g' | awk '{print $4}')
+TF_BINARY="${TF_BINARIES_FOLDER}/terraform${TF_VERSION}"
+
+# https://goo.gl/B37MYs
+export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get -qq install -y unzip \
-&& wget --quiet "${REMOTE_ZIP}" --directory-prefix "${TEMP_FOLDER}" \
-&& mkdir -p "${TERRAFORM_INSTALL_FOLDER}" \
-&& find "${TEMP_FOLDER}" -type f -name *.zip -exec unzip {} -d "${TERRAFORM_INSTALL_FOLDER}" \;
+&& wget --quiet "${TF_ZIP}" --directory-prefix "${TEMP_FOLDER}" \
+&& mkdir -p "${TF_BINARIES_FOLDER}" \
+&& find "${TEMP_FOLDER}" -type f -name *.zip -exec unzip {} -d "${TEMP_FOLDER}" \; \
+&& mv "${TEMP_FOLDER}/terraform" "${TF_BINARY}" \
+&& sudo ln -s "${TF_BINARY}" /usr/local/bin/terraform \
+&& cd /synced/Base/ && /usr/local/bin/terraform init \
+&& sudo cp -r /synced/.aws/ /home/ubuntu/ \
+&& chown ubuntu:ubuntu -R /home/ubuntu/.aws/ && cd /home/ubuntu/.aws / && chmod 600 ./*
