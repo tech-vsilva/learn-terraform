68fb71b7179749fd9f63ba2559b39cb5ffa3b700
Valter Silva <valter.silva@orderbird.com>
2017-08-25 14:36:58 +0200

    Sync folders under ./synced folder into /synced. Copy ~/.aws/ into ubuntu's home directory;

---
+++ synced/Base/base.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region                  = "eu-central-1"
+  shared_credentials_file = "/home/ubuntu/.aws/credentials"
+  profile                 = "dev"
+  version                 = "~> 0.1"
+}
