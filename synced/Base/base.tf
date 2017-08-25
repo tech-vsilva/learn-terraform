5a1e45d0747a57a2903fe1bc0b78c2323407c29b
Valter Silva <valter.silva@orderbird.com>
2017-08-25 16:49:56 +0200

    Replace tag's 'name' value by a variable'

--- synced/Base/base.tf
+++ synced/Base/base.tf
@@ -4,3 +4,12 @@ provider "aws" {
   profile                 = "dev"
   version                 = "~> 0.1"
 }
+
+resource "aws_instance" "ob_example_1" {
+  ami = "ami-1e339e71"
+  instance_type = "t2.micro"
+  tags {
+    name = "${var.ob_server_name}",
+    use  = "An AWS instance created as example"
+  }
+}
---
+++ synced/Base/variables.tf
@@ -0,0 +1,4 @@
+variable "ob_server_name" {
+  default = "Example"
+  description = "A name for the example server."
+}
